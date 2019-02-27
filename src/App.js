a72127388295aba89420045621440912fb7a92e4
asper1969 <aspiosa@gmail.com>
2019-02-27 14:18:30 +0600

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component{
+    state = {
+        dishes: DISHES
+    };
+
     render(){
         return(
-            <div className="App">
-                <Menu/>
-            </div>
+            <Menu dishes={this.state.dishes}/>
         );
     }
 }
