3ba0c53d280fe4c6f46ba97e137eb7fbb715ff13
asper1969 <aspiosa@gmail.com>
2019-02-28 12:46:50 +0600

    Functional Components

--- src/components/CommentsComponent.js
+++ src/components/CommentsComponent.js
@@ -1,54 +1,43 @@
 import React, {Component} from 'react';
 
-class Comments extends Component{
+function RenderComment(comments){
 
-    renderComment(comments) {
+    const commentsList = comments.map((item) => {
 
-        const commentsList = comments.map((item) => {
-
-            const {comment, author, date} = item;
-            const formatedDate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(date)));
-
-            return (
-                <li>
-                    <p className="text">{comment}</p>
-                    <p className="info">
-                        <span className="delimeter mr-2">--</span>
-                        <span className="date mr-2">
-                            {/*{new Intl.DateTimeFormat(*/}
-                                {/*'en-US',*/}
-                                {/*{*/}
-                                    {/*year: 'numeric',*/}
-                                    {/*month: 'short',*/}
-                                    {/*day: '2-digit'*/}
-                                {/*}).format(new Date(Date.parse({date})))*/}
-                            {/*}*/}
-
-                            {formatedDate}
-                        </span>
-                        <span className="name"> {author}</span>
-                    </p>
-                </li>
-            );
-        });
+        const {comment, author, date, id} = item;
+        const formatedDate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(date)));
 
         return (
-            <div>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {commentsList}
-                </ul>
-            </div>
+            <li key={id}>
+                <p className="text">{comment}</p>
+                <p className="info">
+                    <span className="delimeter mr-2">--</span>
+                    <span className="date mr-2">
+                            {formatedDate}
+                        </span>
+                    <span className="name"> {author}</span>
+                </p>
+            </li>
         );
-    }
+    });
 
-    render(){
-        const {comments} = this.props;
+    return (
+        <div>
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {commentsList}
+            </ul>
+        </div>
+    );
+}
 
-        return(
-          this.renderComment(comments)
-        );
-    }
+function Comments(dish){
+
+    const {comments} = dish;
+
+    return(
+        RenderComment(comments)
+    );
 }
 
 export default Comments;
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,38 +2,37 @@ import React, {Component} from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 import Comment from './CommentsComponent';
 
-class Dishdetail extends Component{
+function RenderDish(dish){
 
-    renderDish(dish){
-        const {image,name,description,comments} = dish;
+    const {image,name,description,comments} = dish;
 
-        return(
-            <div class="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" top src={image} alt={name}/>
-                        <CardBody>
-                            <CardTitle>{name}</CardTitle>
-                            <CardText>{description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <Comment comments={comments}/>
-                </div>
+    return(
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" top src={image} alt={name}/>
+                    <CardBody>
+                        <CardTitle>{name}</CardTitle>
+                        <CardText>{description}</CardText>
+                    </CardBody>
+                </Card>
             </div>
-        );
-    };
-
-    render(){
-        const {dish} = this.props;
-
-        return (
-            <div className="container">
-                {this.renderDish(dish)}
+            <div className="col-12 col-md-5 m-1">
+                <Comment comments={comments}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+const Dishdetail = (props) => {
+
+    const {dish} = props;
+
+    return (
+        <div className="container">
+            {RenderDish(dish)}
+        </div>
+    );
+};
 
 export default Dishdetail;
